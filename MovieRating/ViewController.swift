63e8ecb0884509e631e08effee79cba7d43ec10e
hyeri00 <nexusring@gmail.com>
2023-04-03 14:25:35 +0900

    initial commit

---
+++ MovieRating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRating
+//
+//  Created by 혜리 on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
