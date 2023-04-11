7090538bdb8ce9958db548d1f04a6ff884b24ab7
hyeri00 <nexusring@gmail.com>
2023-04-11 23:10:15 +0900

    보관함 수정 중

---
+++ MovieRating/Model/MovieCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieRating
+//
+//  Created by 혜리 on 2023/04/05.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    var thumbnailImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    var evaluationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .orange
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, evaluationLabel])
+        view.spacing = 5
+        view.axis = .vertical
+        view.distribution = .fillEqually
+        view.alignment = .fill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        contentView.addSubview(thumbnailImage)
+        contentView.addSubview(stackView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            thumbnailImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            thumbnailImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            thumbnailImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            thumbnailImage.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -10),
+            
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+}
